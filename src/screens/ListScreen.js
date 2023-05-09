a9bc1a70af7c69d3c5fde9b247e90294643736b1
KushalRB <kushal.rajbhandari@fusemachines.com>
2023-05-09 15:06:43 +0545

    Flat list rendered

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,28 @@
 import React from "react";
 import {View, StyleSheet, Text} from 'react-native';
+import { FlatList } from "react-native-gesture-handler";
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>
+    const friends = [
+        {name : 'Friend #1'},
+        {name : 'Friend #2'},
+        {name : 'Friend #3'},
+        {name : 'Friend #4'},
+        {name : 'Friend #5'},
+        {name : 'Friend #6'},
+        {name : 'Friend #7'},
+        {name : 'Friend #8'},
+        {name : 'Friend #9'},
+        {name : 'Friend #10'}
+
+    ]
+    return (<FlatList 
+        data={friends} 
+        keyExtractor={friend => friend.name}
+        renderItem={({item}) => {
+            return(<Text>{item.name}</Text>);
+        }}>
+    </FlatList>);
 }
 
 const styles = StyleSheet.create({});
