67b778b7474189afdab0aa2a69422453afec62c0
KushalRB <kushal.rajbhandari@fusemachines.com>
2023-05-09 18:53:15 +0545

    Exploredm different props of FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,27 +4,36 @@ import { FlatList } from "react-native-gesture-handler";
 
 const ListScreen = () => {
     const friends = [
-        {name : 'Friend #1'},
-        {name : 'Friend #2'},
-        {name : 'Friend #3'},
-        {name : 'Friend #4'},
-        {name : 'Friend #5'},
-        {name : 'Friend #6'},
-        {name : 'Friend #7'},
-        {name : 'Friend #8'},
-        {name : 'Friend #9'},
-        {name : 'Friend #10'}
+        {name: 'Friend #1', age: '20'},
+        {name: 'Friend #2', age: '23'},
+        {name: 'Friend #3', age: '26'},
+        {name: 'Friend #4', age: '32'},
+        {name: 'Friend #5', age: '33'},
+        {name: 'Friend #6', age: '29'},
+        {name: 'Friend #7', age: '20'},
+        {name: 'Friend #8', age: '28'},
+        {name: 'Friend #9', age: '32'},
+        {name: 'Friend #10', age: '30'}
 
     ]
     return (<FlatList 
         data={friends} 
         keyExtractor={friend => friend.name}
         renderItem={({item}) => {
-            return(<Text>{item.name}</Text>);
+            return(
+            <View>
+                <Text style = {styles.textSyle}>{item.name} - Age {item.age}</Text>
+                
+            </View>
+            );
         }}>
     </FlatList>);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textSyle: {
+        marginVertical: 20
+    }
+});
 
 export default ListScreen;
