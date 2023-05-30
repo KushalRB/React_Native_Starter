9c3a445d8091ec74012d9ece109214cb4ecd5c3f
KushalRB <kushal.rajbhandari@fusemachines.com>
2023-05-30 13:35:29 +0545

    Navigating wiht React Navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, Touchable } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = (props) => {
+  console.log('the props is', props.navigation)
+  return  (
+  <View>
+    <Button 
+      title = "Go to Components Demo"
+      onPress={() => props.navigation.navigate('Components')}
+    />
+    <TouchableOpacity 
+      onPress={() => props.navigation.navigate('List')}>
+      <Text style = {styles.text}>Go to List Demo</Text>
+    </TouchableOpacity>
+  </View>
+  
+  );
 };
 
 const styles = StyleSheet.create({
