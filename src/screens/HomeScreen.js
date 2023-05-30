497a0f7972c3fce12899717d5a8d6b3ba8e73be3
KushalRB <kushal.rajbhandari@fusemachines.com>
2023-05-30 17:04:15 +0545

    Section 4 (#3)
    * List screen created
    
    * Flat list rendered
    
    * Exploredm different props of FlatList
    
    * Navigating wiht React Navigation
    
    * Destructuring props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, Touchable } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({navigation}) => {
+  return  (
+  <View>
+    <Button 
+      title = "Go to Components Demo"
+      onPress={() => navigation.navigate('Components')}
+    />
+    <Button 
+      title = "Go to List Demo"
+      onPress={() => navigation.navigate('List')}
+    />
+  </View>
+  
+  );
 };
 
 const styles = StyleSheet.create({
